415f00b693866c88afbba9881c1a21ca495b25b6
Jeremy Magland <jmagland@flatironinstitute.org>
2022-09-17 08:50:58 -0400

    v 0.1.3

--- dist/component-tab-widget/TabWidget.js
+++ dist/component-tab-widget/TabWidget.js
@@ -13,10 +13,10 @@ const TabWidget = ({ children, tabs, width, height }) => {
     const vMargin = 8;
     const W = (width || 300) - hMargin * 2;
     const H = height - vMargin * 2;
-    return (_jsx("div", Object.assign({ style: { position: 'absolute', left: hMargin, top: vMargin, width: W, height: H, overflow: 'hidden' }, className: "TabWidget" }, { children: _jsxs("div", Object.assign({ style: { position: 'absolute', left: 0, top: 0, width: W, height: tabBarHeight } }, { children: [_jsx(TabWidgetTabBar, { tabs: tabs, currentTabIndex: currentTabIndex, onCurrentTabIndexChanged: setCurrentTabIndex, onTabClosed: () => { } }), children2.map((c, i) => {
-                    const visible = i === currentTabIndex;
-                    return (_jsx("div", Object.assign({ style: { visibility: visible ? 'visible' : 'hidden', overflowY: 'hidden', overflowX: 'hidden', position: 'absolute', left: 0, top: tabBarHeight, width: W, height: H } }, { children: _jsx(c.type, Object.assign({}, c.props, { width: W, height: H - tabBarHeight })) }), `child-${i}`));
-                })] }), "tabwidget-bar") })));
+    return (_jsxs("div", Object.assign({ style: { position: 'absolute', left: hMargin, top: vMargin, width: W, height: H, overflow: 'hidden' }, className: "TabWidget" }, { children: [_jsx("div", Object.assign({ style: { position: 'absolute', left: 0, top: 0, width: W, height: tabBarHeight } }, { children: _jsx(TabWidgetTabBar, { tabs: tabs, currentTabIndex: currentTabIndex, onCurrentTabIndexChanged: setCurrentTabIndex, onTabClosed: () => { } }) }), "tabwidget-bar"), children2.map((c, i) => {
+                const visible = i === currentTabIndex;
+                return (_jsx("div", Object.assign({ style: { visibility: visible ? 'visible' : 'hidden', overflowY: 'hidden', overflowX: 'hidden', position: 'absolute', left: 0, top: tabBarHeight, width: W, height: H } }, { children: _jsx(c.type, Object.assign({}, c.props, { width: W, height: H - tabBarHeight })) }), `child-${i}`));
+            })] })));
 };
 export default TabWidget;
 //# sourceMappingURL=TabWidget.js.map
